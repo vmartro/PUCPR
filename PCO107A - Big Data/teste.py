21b4d5f7b93cf786b3709b687750a11c38f9a0ee
vmartro <matheusvicentemartinscastro@gmail.com>
2026-03-05 10:59:17 -0300

    Adição de Projeto - Java

---
+++ PCO107A - Big Data/teste.py
@@ -0,0 +1,14 @@
+a = 0
+b = 0
+
+oratorio = a + b
+
+while oratorio != 200:
+    print(oratorio)
+    oratorio += 1
+    if oratorio == 19*10:
+        break
+    else:
+        continue
+
+print(oratorio)
