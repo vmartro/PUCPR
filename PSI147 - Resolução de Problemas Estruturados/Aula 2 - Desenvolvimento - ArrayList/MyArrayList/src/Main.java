93c970a6c951a2d551cb037f104ceb0a0606e3a6
vmartro <matheusvicentemartinscastro@gmail.com>
2026-03-12 19:09:53 -0300

    Projeto - MyArrayList

--- PSI147 - Resolução de Problemas Estruturados/Aula 2 - Desenvolvimento - ArrayList/MyArrayList/src/Main.java
+++ PSI147 - Resolução de Problemas Estruturados/Aula 2 - Desenvolvimento - ArrayList/MyArrayList/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         ArrayIndexList a = new ArrayIndexList();
-        a.add(1, 2);
-
+        a.add(0, 1);
+        a.remove(0);
     }
 }
